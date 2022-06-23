b9aea37ed84f8da7541e30e7c1b02c5462f09a49
ousseini16 <106655694+ousseini16@users.noreply.github.com>
2022-06-23 13:47:56 +0200

    message pertinent

---
+++ src/Metier/Principale.java
@@ -0,0 +1,10 @@
+package Metier;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		Student lr = new Student(1022, "Ousseini",  "Yadiga");
+		System.out.println(lr);
+	}
+
+}
